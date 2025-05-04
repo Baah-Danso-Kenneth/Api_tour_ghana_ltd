cf5d09be28e94c626cbe1732cf2131bec4ac8119
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-05-04 09:10:21 +0000

    Ammend logic for payment serializers and viewsets

--- core/payments/serializers.py
+++ core/payments/serializers.py
@@ -6,8 +6,8 @@ import uuid
 class LightningPaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model=LightningPayment
-        invoice_id = f"inv_{uuid.uuid4().hex[:10]}"
         fields = ['invoice_id', 'amount_in_sats', 'paid', 'created_at', 'paid_at', 'payment_type']
+        read_only_fields = ['invoice_id', 'paid', 'created_at', 'paid_at']
 
 
 
@@ -17,3 +17,13 @@ class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = ['id', 'product', 'quantity', 'total_amount_sats', 'payment', 'paid', 'created_at']
+
+    def create(self, validated_data):
+        payment_data = validated_data.pop('payment')
+
+        lightning_payment = LightningPayment.objects.create(
+                invoice_id=f"inv_{uuid.uuid4().hex[:10]}",
+                **payment_data
+            )
+        order = Order.objects.create(payment=lightning_payment)
+        return order
--- core/payments/viewsets.py
+++ core/payments/viewsets.py
@@ -13,27 +13,3 @@ class LightningPaymentViewset(viewsets.ModelViewSet):
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
-
-    def create(self, request, *args, **kwargs):
-        # Extract order and payment data from the request
-        order_data = request.data.get('order')
-        lightning_payment_data = order_data.get('payment')
-
-        # Create Lightning Payment (dynamically generate invoice_id in a real-world scenario)
-        lightning_payment = LightningPayment.objects.create(
-            invoice_id="unique_invoice_id",  # Dynamic generation required here
-            amount_in_sats=lightning_payment_data['amount_in_sats'],
-            payment_type=lightning_payment_data['payment_type'],
-        )
-
-        # Create Order
-        order = Order.objects.create(
-            product_id=order_data['product'],  # Assuming product ID is passed in the data
-            quantity=order_data['quantity'],
-            total_amount_sats=order_data['total_amount_sats'],
-            payment=lightning_payment
-        )
-
-        # Return the created order as a response
-        serializer = OrderSerializer(order)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
