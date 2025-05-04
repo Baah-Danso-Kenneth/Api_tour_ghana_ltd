558803a8b32d5bc02dd59cf91c1da14a00b324f5
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-05-04 20:39:33 +0000

    Ensure viewsets are logically correct for lightning connection

--- core/payments/viewsets.py
+++ core/payments/viewsets.py
@@ -1,15 +1,32 @@
-from rest_framework import viewsets
-from rest_framework import status
+from rest_framework import viewsets, status
 from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
 from .models import Order, LightningPayment
 from .serializers import OrderSerializer, LightningPaymentSerializer
 
-
-class LightningPaymentViewset(viewsets.ModelViewSet):
-    queryset= LightningPayment.objects.all()
+class LightningPaymentViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    You probably want LightningPayment to be read-only from the API,
+    since it's generated internally when an order is created.
+    """
+    queryset = LightningPayment.objects.all()
     serializer_class = LightningPaymentSerializer
 
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+
+    def create(self, request, *args, **kwargs):
+        """
+        Override create to catch serializer validation errors cleanly.
+        """
+        serializer = self.get_serializer(data=request.data)
+
+        try:
+            serializer.is_valid(raise_exception=True)
+        except ValidationError as e:
+            return Response({'errors': e.detail}, status=status.HTTP_400_BAD_REQUEST)
+
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
